c18496ae2265e097ca3cfbbee6e388749b67a965
Bobin_Vyacheslav <bobin.vyacheslav@yandex.ru>
2025-10-26 16:14:11 +0300

    Interest class added

---
+++ src/main/java/BasicClasses/Interest.java
@@ -0,0 +1,11 @@
+package BasicClasses;
+
+import lombok.Data;
+
+@Data
+public class Interest {
+  private String name;
+  private String category;
+  private String description;
+  private Integer id;
+}
