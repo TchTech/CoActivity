deba514cad96a0a130f9625aa1259b2919e99680
Bobin_Vyacheslav <bobin.vyacheslav@yandex.ru>
2025-11-02 13:32:34 +0300

    Fix naming in User.java

--- src/main/java/BasicClasses/User.java
+++ src/main/java/BasicClasses/User.java
@@ -1,17 +1,17 @@
 package BasicClasses;
 import lombok.Data;
 
+import java.time.Instant;
 import java.util.List;
-import java.util.Date;
 @Data
 public class User {
   private String name;
   private String eMail;
   private Integer id;
   private String passwordHash;
-  private List<Integer> rooms;
-  private List<Integer> interests;
-  private List<Integer> feedbacks;
-  private List<Integer> feedbacksAuthor;
-  private Date createdAt;
+  private List<Room> rooms;
+  private List<Interest> interests;
+  private List<Feedback> feedbacks;
+  private List<Feedback> feedbacksAuthor;
+  private Instant createdAt;
 }
