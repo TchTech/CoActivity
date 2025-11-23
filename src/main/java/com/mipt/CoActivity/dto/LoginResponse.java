b30c2058ef688d884c79f756a505e1fffbdecc7d
Озолинь Артур Айварович <ozolin.aa@phystech.edu>
2025-11-23 10:56:53 +0300

    created login and registration

---
+++ src/main/java/com/mipt/CoActivity/dto/LoginResponse.java
@@ -0,0 +1,21 @@
+package com.mipt.CoActivity.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Response after successful login")
+public class LoginResponse {
+    @Schema(description = "JWT access token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+    private String token;
+
+    @Schema(description = "User ID", example = "1")
+    private Long userId;
+}
+
